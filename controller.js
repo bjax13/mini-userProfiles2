9d08145a8cb64ea3f04fe662bc007513fe38a3ad
Bryan Jackson <bjack13@gmail.com>
2016-12-14 12:40:31 -0700

    me following along with jermy

--- controller.js
+++ controller.js
@@ -1,11 +1,27 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-    mainService.getUsers().then(function (response) {
-      $scope.users = response.data.data;
-    })
+  $scope.currentPage = 1;
 
+  $scope.getUsers = function() {
+    var promise = mainService.getUsers($scope.currentPage)
+
+    promise.then(function (people) {
+      $scope.users = people;
+    });
   }
   $scope.getUsers();
+
+  $scope.prev = function () {
+    if ($scope.currentPage > 1) {
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+
+  }
+  $scope.next = function () {
+    $scope.currentPage++;
+    $scope.getUsers();
+
+  }
 
 });
--- service.js
+++ service.js
@@ -2,11 +2,19 @@ angular.module('userProfiles').service('mainService', function($http) {
 
 
 
-	this.getUsers = function () {
-		return $http({
+	this.getUsers = function (pgNum) {
+
+		var promise = $http({
 			method: 'GET',
-			url: 'http://reqres.in/api/users?page=1'
-		})
+			url: 'http://reqres.in/api/users?page='+pgNum
+		}).then(function (response) {
+			if (response.status === 200) {
+					return response.data.data;
+			}
+			return "error"
+		});
+
+		return promise;
 	}
 
 
